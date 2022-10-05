83247576c2a23660ac86c4cfa7ec3bcdec06f23e
yawai990 <ywaung990@gmail.com>
2022-10-05 19:49:00 +0630

    finilize

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:5000';
+const URL = 'https://portfoliodashboard22v2.herokuapp.com';
 
 export const fetchProjects =()=>axios.get(`${URL}/projects/all_projects`);
 export const fetchSkill =()=>axios.get(`${URL}/languages/get_languages`);
